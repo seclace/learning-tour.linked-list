06ff3e5f579eedb399ec12181e344c8c9466fdd7
Левин Олег Евгеньевич <levishok@gmail.com>
2016-09-18 02:23:46 +0700

    Так стало лучше

--- Learning-tour.linked-lists/Program.cs
+++ Learning-tour.linked-lists/Program.cs
@@ -1,26 +1,21 @@
 ﻿using System;
 
-namespace Learning_tour.linked_lists
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Learning_tour.linked_lists {
+    class Program {
+        static void Main(string[] args) {
             LinkedList list = new LinkedList();
-            list.addNode("Первый узел");
-            list.addNode("Second node");
-            list.addNode("Tres node");
-            list.addNode("4 node");
+            list.add("Первый узел");
+            list.add("Second node");
+            list.add("Tres node");
+            list.add("4 node");
 
-            for (int i = 0; i < list.count; )
-            {
-                Console.WriteLine(list.getNodeByIndex(i++).data);
+            for (int i = 0; i < list.count;) {
+                Console.WriteLine(list.get(i++).data);
             }
 
-            list.addNodeByIndex("Temp data", 2);
+            list.add("Temp data", 2);
 
-            for (int i = 0; i < list.count; )
-            {
+            for (int i = 0; i < list.count;) {
                 Console.WriteLine(list[i++].data);
             }
 
--- Learning-tour.linked-lists/linked-list-data/node.cs
+++ Learning-tour.linked-lists/linked-list-data/node.cs
@@ -1,54 +1,26 @@
-﻿namespace Learning_tour.linked_lists
-{
-    class node
-    {
+﻿namespace Learning_tour.linked_lists {
+    class node {
         public object data { get; set; }
-        node next = null;
-        node prev = null;
+        public node next { get; set; }
+        public node prev { get; set; }
 
-        public node() { }
+        public node() { next = prev = null; }
 
-        public node(object _data)
-        {
+        public node(object _data) {
             data = _data;
+            next = prev = null;
         }
 
-        public node(object _data, node _prev, node _next)
-        {
+        public node(object _data, node _prev, node _next) {
             next = _next;
             prev = _prev;
             data = _data;
         }
 
-        public node(node _node)
-        {
+        public node(node _node) {
             data = _node.data;
             next = _node.next;
             prev = _node.prev;
-        }
-
-        public void setNextNode(node input)
-        {
-            next = input;
-        }
-        public void setPrevNode(node input)
-        {
-            prev = input;
-        }
-
-        public void setPrevAndNextNodes(node _prev, node _next)
-        {
-            setPrevNode(_prev);
-            setNextNode(_next);
-        }
-
-        public node getNextNode()
-        {
-            return next;
-        }
-        public node getPrevNode()
-        {
-            return prev;
         }
     }
 }
